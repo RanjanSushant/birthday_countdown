f3a49fbbc89efe109f6a2614939eb9d7ba9f73d6
Sushant Ranjan <er.sushantranjan@gmail.com>
2022-01-21 13:02:33 +0530

    diff zero updated

--- app.js
+++ app.js
@@ -11,9 +11,14 @@ function setText() {
   const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((diff % (1000 * 60)) / 1000);
-  
-  document.querySelector(".birthday-title").innerHTML = `
-    <h1> ${days} days : ${hours} hours : ${minutes}minutes : ${seconds} seconds</h1`;
+
+  if (seconds === 0) {
+    document.querySelector(".birthday-title").innerHTML = `
+    <h1>🥳IT'S YOUR BIRTHDAY 🥳</h1>`;
+  } else {
+    document.querySelector(".birthday-title").innerHTML = `
+    <h1> ${days} days : ${hours} hours : ${minutes}minutes : ${seconds} seconds</h1>`;
+  }
 }
 
 setInterval(setText, 1000);
