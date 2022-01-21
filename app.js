9c9cd19d24c22f8077d8a505ce6c8c9394addef3
Sushant Ranjan <er.sushantranjan@gmail.com>
2022-01-21 12:28:42 +0530

    updated all files

---
+++ app.js
@@ -0,0 +1,21 @@
+const date = new Date();
+const countDownDate = new Date("Feb 3 2022, 00:00:00");
+
+console.log(date.toLocaleString("en-IN"));
+console.log(countDownDate);
+
+function setText() {
+  const diff = countDownDate - new Date();
+
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+  
+  document.querySelector(".birthday-title").innerHTML = `
+    <h1> ${days} days : ${hours} hours : ${minutes}minutes : ${seconds} seconds</h1`;
+}
+
+setInterval(setText, 1000);
+
+console.log(seconds);
